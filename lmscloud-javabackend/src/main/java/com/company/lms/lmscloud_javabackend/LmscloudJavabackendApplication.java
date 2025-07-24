685705eebb9d194999ceed08da96ca9b94c6a852
Gana498 <62788810+Gana498@users.noreply.github.com>
2025-07-24 09:47:52 +0000

    Created Project Structure: Frontend (ReactJS): lmscloud-ui Backend (Spring Boot): lmscloud-javabackend

---
+++ lmscloud-javabackend/src/main/java/com/company/lms/lmscloud_javabackend/LmscloudJavabackendApplication.java
@@ -0,0 +1,13 @@
+package com.company.lms.lmscloud_javabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmscloudJavabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmscloudJavabackendApplication.class, args);
+	}
+
+}
---
+++ lmscloud-javabackend/src/test/java/com/company/lms/lmscloud_javabackend/LmscloudJavabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.lms.lmscloud_javabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmscloudJavabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
